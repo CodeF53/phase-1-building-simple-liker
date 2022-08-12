3c1de1e13155a7bfa593fbcc6c334f9ad2629885
ChaseWelsh <fseusb@gmail.com>
2022-08-12 13:26:12 -0600

    commit tree 93d59736d066b104566321e38c49085550f577b6

--- main.js
+++ main.js
@@ -3,9 +3,38 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+// Error Management
+function resetErrorModal() {
+  document.querySelector("#modal").classList.add("hidden")
+  document.querySelector("#modal-message").textContent = ""
+}
+function displayError(error) {
+  document.querySelector("#modal").classList.remove("hidden")
+  document.querySelector("#modal-message").textContent = error;
+}
+// hide our error modal by default.
+resetErrorModal()
 
 
-
+// Like Mangement
+document.body.addEventListener("mouseup", (event) => {
+  let heart_node = event.target
+  switch (heart_node.textContent){
+    case EMPTY_HEART:
+      mimicServerCall(url="http://mimicServer.example.com").then(()=>{
+        heart_node.textContent = FULL_HEART;
+        heart_node.classList.add("activated-heart")
+      }).catch((error)=>{
+        displayError(error)
+      })
+      break;
+    case FULL_HEART:
+      // kinda odd that we dont make a server request to unlike
+      heart_node.textContent = EMPTY_HEART;
+      heart_node.classList.remove("activated-heart")
+      break;
+  }
+})
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
